f9918d0af9b4ff9350fa69c6676249c4e9fd1c1f
Tyler Doster <tyler.j.doster@gmail.com>
2020-04-13 22:34:34 -0700

    show app status in mainactivity

--- app/src/main/java/com/dapathy/togglemaps/MainActivity.java
+++ app/src/main/java/com/dapathy/togglemaps/MainActivity.java
@@ -1,35 +1,45 @@
 package com.dapathy.togglemaps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.admin.DeviceAdminReceiver;
-import android.app.admin.DevicePolicyManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.topjohnwu.superuser.Shell;
-import com.topjohnwu.superuser.ShellUtils;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+	private final static String Package_Name = "com.google.android.apps.maps";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		try {
+			setStatusTextView();
+		} catch (PackageManager.NameNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void onToggleMapsClick(View view) throws PackageManager.NameNotFoundException {
-		final String packageName = "com.google.android.apps.maps";
-		togglePackage(packageName);
+		togglePackage();
+		setStatusTextView();
 	}
 
-	private void togglePackage(String packageName) throws PackageManager.NameNotFoundException {
-		ApplicationInfo applicationInfo = getApplicationContext().getPackageManager().getApplicationInfo(packageName, 0);
-		String command = applicationInfo.enabled ? "disable" : "enable";
-		Shell.su(String.format("pm %s %s", command, packageName)).exec();
+	private void togglePackage() throws PackageManager.NameNotFoundException {
+		String command = getPackageStatus() ? "disable" : "enable";
+		Shell.su(String.format("pm %s %s", command, Package_Name)).exec();
+	}
+
+	private void setStatusTextView() throws PackageManager.NameNotFoundException {
+		TextView textView = findViewById(R.id.package_status);
+		String status = getPackageStatus() ? "enabled" : "disabled";
+		textView.setText(String.format("App is: %s", status));
+	}
+
+	private boolean getPackageStatus() throws PackageManager.NameNotFoundException {
+		return getApplicationContext().getPackageManager().getApplicationInfo(Package_Name, 0).enabled;
 	}
 }
