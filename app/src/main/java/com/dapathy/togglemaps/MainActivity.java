9a4b2d624f6dec7bafe151e0b17777645af171f3
Tyler Doster <tyler.j.doster@gmail.com>
2020-04-13 21:43:16 -0700

    button to toggle maps using root

--- app/src/main/java/com/dapathy/togglemaps/MainActivity.java
+++ app/src/main/java/com/dapathy/togglemaps/MainActivity.java
@@ -2,7 +2,17 @@ package com.dapathy.togglemaps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
+
+import com.topjohnwu.superuser.Shell;
+import com.topjohnwu.superuser.ShellUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+	}
+
+	public void onToggleMapsClick(View view) throws PackageManager.NameNotFoundException {
+		final String packageName = "com.google.android.apps.maps";
+		togglePackage(packageName);
+	}
+
+	private void togglePackage(String packageName) throws PackageManager.NameNotFoundException {
+		ApplicationInfo applicationInfo = getApplicationContext().getPackageManager().getApplicationInfo(packageName, 0);
+		String command = applicationInfo.enabled ? "disable" : "enable";
+		Shell.su(String.format("pm %s %s", command, packageName)).exec();
 	}
 }
